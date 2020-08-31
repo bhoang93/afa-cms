ebd9ce735669c39455fdfaf61b7e7d0f4933b012
bhoang35 <bhoang35@gmail.com>
2020-08-31 20:05:45 +0100

    cms

--- src/templates/contact-page.js
+++ src/templates/contact-page.js
@@ -3,32 +3,49 @@ import ContactForm from "../components/ContactForm";
 import email from "../img/Contact/mail4.svg";
 import phone from "../img/Contact/phone.svg";
 import Wrapper from "../components/AppWrapper";
+import { graphql } from "gatsby";
 
-const Contact = () => {
+const Contact = ({ data }) => {
+  const number = data.contact.edges[0].node.frontmatter.phone;
+  const mail = data.contact.edges[0].node.frontmatter.email;
+
   return (
     <Wrapper>
       <div className="contact">
         <h2 className="sub-heading">Contact Us</h2>
-        <a className="contact__link" href="tel:07838926678">
+        <a className="contact__link" href={`tel:${number}`}>
           <div className="contact__block">
             <img src={phone} className="contact__block--img" alt="phone" />
-            <p className="contact__link--text"> (+44) 07838926678</p>
+            <p className="contact__link--text"> (+44) {number}</p>
           </div>
         </a>
-        <a
-          className="contact__link"
-          href="mailto:info@advocates-for-animals.com"
-        >
+        <a className="contact__link" href={`mailto:${mail}`}>
           <div className="contact__block">
             <img src={email} className="contact__block--img" alt="email" />
-            <p className="contact__link--text">
-              info@advocates-for-animals.com
-            </p>
+            <p className="contact__link--text">{mail}</p>
           </div>
         </a>
       </div>
     </Wrapper>
   );
 };
+
+export const pageQuery = graphql`
+  query Contact {
+    contact: allMarkdownRemark(
+      filter: { fileAbsolutePath: { regex: "/contact/" } }
+    ) {
+      edges {
+        node {
+          excerpt
+          frontmatter {
+            email
+            phone
+          }
+        }
+      }
+    }
+  }
+`;
 
 export default Contact;
