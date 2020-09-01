abb494d7276689c93819b4c5df0f5d9801cead9b
bhoang35 <bhoang35@gmail.com>
2020-09-01 11:22:33 +0100

    cms

---
+++ src/components/BlogContainer.js
@@ -0,0 +1,53 @@
+import React from "react";
+import { StaticQuery } from "gatsby";
+import { Scrollbars } from "react-custom-scrollbars";
+import BlogPost from "./BlogPost";
+
+const BlogContainer = () => {
+  return (
+    <div className="news-blog-container">
+      <StaticQuery
+        query={graphql`
+          query BlogPlQuery {
+            allMarkdownRemark(
+              sort: { order: DESC, fields: [frontmatter___date] }
+              filter: { frontmatter: { templateKey: { eq: "blog-post" } } }
+            ) {
+              edges {
+                node {
+                  excerpt(pruneLength: 400)
+                  id
+                  fields {
+                    slug
+                  }
+                  frontmatter {
+                    title
+                    templateKey
+                    date(formatString: "MMMM DD, YYYY")
+                    featuredpost
+                    featuredimage {
+                      childImageSharp {
+                        fluid(maxWidth: 120, quality: 100) {
+                          ...GatsbyImageSharpFluid
+                        }
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        `}
+        render={(data, count) => (
+          <Scrollbars style={{ height: 600 }}>
+            {data.allMarkdownRemark.edges.map((post, i) => {
+              return <BlogPost post={post.node} key={i} />;
+            })}
+          </Scrollbars>
+        )}
+      />
+    </div>
+  );
+};
+
+export default BlogContainer;
